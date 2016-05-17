c54aeb35818846f4094bf3b37116a99c4e7e53ba
Zachary Weston <zakweston@Zacharys-MacBook-Pro.local>
2016-05-16 22:51:41 -0400

    First draft

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // htts://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  // A pretty standard set of linting rules for JavaScript
+  extends: 'standard',
+  // Required to lint JavaScript inside script tags in HTML files
+  plugins: [ 'html' ]
+}
